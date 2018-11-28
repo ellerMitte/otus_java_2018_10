c968fc66cf18bc17bb710e7767da754688c9b571
Igor <eller_mitte@mail.ru>
2018-11-28 12:30:53 +0300

    done hw

--- HW3.1-arrayList/src/main/java/Main.java
+++ HW3.1-arrayList/src/main/java/Main.java
@@ -1,10 +1,37 @@
+
 import java.util.Collections;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        Random random = new Random();
         MyArrayList<Integer> myArray1 = new MyArrayList<>();
-        MyArrayList<Integer> myArray2 = new MyArrayList<>();
-        Collections.addAll(myArray1, 1,2,2);
+        MyArrayList<Integer> myArray2 = new MyArrayList<>(50);
+
+        Integer[] elements1 = new Integer[100];
+        Integer[] elements2 = new Integer[100];
+
+        for (int i=0; i<100; i++) {
+            elements1[i] = random.nextInt(100);
+            elements2[i] = random.nextInt(100);
+        }
+
+        Collections.addAll(myArray1, elements1);
+        Collections.addAll(myArray2, elements2);
+        System.out.println(myArray1);
+        System.out.println(myArray2);
+
+        Collections.copy(myArray1, myArray2);
+        System.out.println(myArray1);
+        System.out.println(myArray2);
+
+        Collections.sort(myArray1);
+        Collections.sort(myArray2, (o1, o2) -> (o1 < o2) ? 1 : ((o1 == o2) ? 0 : -1));
+        System.out.println(myArray1);
+        System.out.println(myArray2);
+
+
     }
 }
--- HW3.1-arrayList/src/main/java/MyArrayList.java
+++ HW3.1-arrayList/src/main/java/MyArrayList.java
@@ -3,22 +3,153 @@ import java.util.*;
 public class MyArrayList<T> implements List<T> {
 
     private static final int DEFAULT_CAPACITY = 10;
-    private static final Object[] EMPTY_ELEMENTDATA = {};
-    private static final Object[] DEFAULTCAPACITY_EMPTY_ELEMENTDATA = {};
+    private static final int MAX_ARRAY_SIZE = Integer.MAX_VALUE - 8;
     private Object[] elementData;
     private int size;
 
     public MyArrayList() {
-        this.elementData = DEFAULTCAPACITY_EMPTY_ELEMENTDATA;
+        this.elementData = new Object[DEFAULT_CAPACITY];
+    }
+
+    public MyArrayList(int size) {
+        if (size > 0) {
+            this.elementData = new Object[size];
+        } else if (size == 0) {
+            this.elementData = new Object[DEFAULT_CAPACITY];
+        } else {
+            throw new IllegalArgumentException("Illegal Capacity: " + size);
+        }
     }
 
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public boolean add(T e) {
+        checkCapacity(size + 1);
+        elementData[size++] = e;
+        return true;
+    }
+
+    private void checkCapacity(int minCapacity) {
+        if (minCapacity > elementData.length) {
+            int oldCapacity = elementData.length;
+            int newCapacity = oldCapacity + (oldCapacity >> 1);
+            if (newCapacity < minCapacity)
+                newCapacity = minCapacity;
+            if (newCapacity > MAX_ARRAY_SIZE)
+                throw new OutOfMemoryError();
+
+            elementData = Arrays.copyOf(elementData, newCapacity);
+        }
+    }
+
+    @Override
+    public T get(int index) {
+        if (index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        return (T) elementData[index];
+    }
+
+    @Override
+    public T set(int index, T element) {
+        if (index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        T oldValue = (T) elementData[index];
+        elementData[index] = element;
+        return oldValue;
+    }
+
+    @Override
+    public void sort(Comparator<? super T> c) {
+        Arrays.sort((T[]) elementData, 0, size, c);
+    }
+
+    @Override
+    public ListIterator<T> listIterator() {
+        return new ListItr(0);
+    }
+
+    @Override
+    public ListIterator<T> listIterator(int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index: " + index);
+        }
+        return new ListItr(index);
+    }
+
+    private class ListItr implements ListIterator<T> {
+
+        int cursor;
+
+        ListItr(int index) {
+            cursor = index;
+        }
+
+        @Override
+        public T next() {
+            int i = cursor;
+            cursor = i + 1;
+            return (T) elementData[i];
+        }
+
+        @Override
+        public void set(T e) {
+            MyArrayList.this.set(cursor - 1, e);
+        }
+
+        @Override
+        public boolean hasNext() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public boolean hasPrevious() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public T previous() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public int nextIndex() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public int previousIndex() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public void add(T e) {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(elementData);
+    }
+
+    @Override
+    public Object[] toArray() {
         throw new UnsupportedOperationException();
     }
 
@@ -33,20 +164,8 @@ public class MyArrayList<T> implements List<T> {
     }
 
     @Override
-    public Object[] toArray() {
-        return new Object[0];
-    }
-
-    @Override
     public <T1> T1[] toArray(T1[] a) {
         throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean add(T e) {
-        //ensureCapacityInternal(size + 1);  // Increments modCount!!
-        elementData[size++] = e;
-        return true;
     }
 
     @Override
@@ -85,18 +204,6 @@ public class MyArrayList<T> implements List<T> {
     }
 
     @Override
-    public T get(int index) {
-        //rangeCheck(index);
-
-        return (T) elementData[index];
-    }
-
-    @Override
-    public T set(int index, T element) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
     public void add(int index, T element) {
         throw new UnsupportedOperationException();
     }
@@ -117,17 +224,8 @@ public class MyArrayList<T> implements List<T> {
     }
 
     @Override
-    public ListIterator<T> listIterator() {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public ListIterator<T> listIterator(int index) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
     public List<T> subList(int fromIndex, int toIndex) {
         throw new UnsupportedOperationException();
     }
+
 }
