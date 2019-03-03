22babc98d2a0637b7caf9eba5f953d3257de48b2
lokoscum <lokoscum@mail.ru>
2019-03-03 15:27:27 +0300

    change hw 6.1

--- HW6.1-cache/src/main/java/cacheForHW/HWCacheDemo.java
+++ HW6.1-cache/src/main/java/cacheForHW/HWCacheDemo.java
@@ -6,17 +6,17 @@ package cacheForHW;
  */
 public class HWCacheDemo {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         new HWCacheDemo().demo();
     }
 
-    private void demo() throws InterruptedException {
+    private void demo() {
         HwCache<Integer, Integer> cache = new MyCache<>();
         HwListener<Integer, Integer> listener =
                 (key, value, action) -> System.out.println("key:" + key + ", value:" + value + ", action:" + action);
         cache.addListener(listener);
         cache.put(1, 1);
-
+        System.out.println(cache.get(1));
         cache.remove(1);
         cache.removeListener(listener);
 
