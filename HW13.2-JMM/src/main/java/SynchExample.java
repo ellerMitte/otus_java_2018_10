d78160e8aac56f3c73cd9c07c9baeb8de9334cea
lokoscum <lokoscum@mail.ru>
2019-03-13 21:39:15 +0300

    add hw 13.2

---
+++ HW13.2-JMM/src/main/java/SynchExample.java
@@ -0,0 +1,46 @@
+/**
+ * @author Igor on 13.03.19.
+ */
+public class SynchExample {
+    private static String thr = "t2";
+
+    public static void main(String[] args) {
+        SynchExample exercise = new SynchExample();
+        new Thread(() -> exercise.action("t1")).start();
+        new Thread(() -> exercise.action("t2")).start();
+    }
+
+    private synchronized void action(String message) {
+        boolean inc = false;
+        int i = 1;
+        StringBuilder sb = new StringBuilder(message + ":");
+        while (true) {
+            if (thr.equals(message)) {
+                wait(this);
+            } else {
+                inc = (i == 10 || i == 1) != inc;
+                sb.append(" ").append(inc ? i++ : i--);
+                System.out.println(sb.toString());
+                thr = message;
+                sleep(1000);
+                notifyAll();
+            }
+        }
+    }
+
+    private static void wait(Object object) {
+        try {
+            object.wait();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
